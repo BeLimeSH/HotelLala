487da9b3620ad6150a69d1191ecbabc269898dac
DESKTOP-IJ8C807\user1 <qkrwngus839412@gmail.com>
2022-06-13 14:04:10 +0900

    수정

--- src/main/java/edu/kh/hotellala/reservationCheck/model/dao/ReservationCheckDAO.java
+++ src/main/java/edu/kh/hotellala/reservationCheck/model/dao/ReservationCheckDAO.java
@@ -1,5 +1,31 @@
-package edu.kh.hotellala.reservationCheck.model.dao;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+
 
 public class ReservationCheckDAO {
+
+	private Statement stmt;
+	private PreparedStatement pstmt;
+	private ResultSet rs;	
+	private Properties prop;
+	
+	public ReservationCheckDAO(){
+		try {
+			prop = new Properties();
+			
+			String filePath = ReservationCheckDAO.class.getResource("/edu/kh/community/sql/rsvCheck-sql.xml").getPath();
+			
+			prop.loadFromXML( new FileInputStream(filePath) );
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 }
--- src/main/java/edu/kh/hotellala/reservationCheck/model/service/ReservationCheckService.java
+++ src/main/java/edu/kh/hotellala/reservationCheck/model/service/ReservationCheckService.java
@@ -1,5 +1,29 @@
-package edu.kh.hotellala.reservationCheck.model.service;
+import static edu.kh.hotellala.common.JDBCTemplate.getConnection;
 
-public class ReservationCheckService {
+import java.sql.Connection;
 
+import edu.kh.hotellala.reservationCheck.model.vo.ReservationCheck;
+import edu.kh.hotellala.reservationCheck.model.dao.ReservationCheckDAO;
+
+public class ReservationCheckService{
+	
+	private ReservationCheckDAO dao = new ReservationCheckDAO();
+	
+	public ReservationCheck reserveCheck(int reservationNo) throws Exception{
+		
+		Connection conn = getConnection();
+		
+		//ReservationCheck rsvCheck = dao.reserveCheck(conn, reservationNo);
+		
+		
+		
+		
+		
+		return null;
+		
+		
+	}
+
+
+	
 }
