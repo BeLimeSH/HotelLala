37adbc9d657a639466712661370b83f2215f0851
1994kg <qkrwngus839412@gmail.com>
2022-06-14 22:16:16 +0900

    dao 수정

--- src/main/java/edu/kh/hotellala/reservationCheck/model/dao/ReservationCheckDAO.java
+++ src/main/java/edu/kh/hotellala/reservationCheck/model/dao/ReservationCheckDAO.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import edu.kh.hotellala.reservation.model.dao.ReservationRequestDAO;
 import edu.kh.hotellala.reservationCheck.model.vo.ReservationCheck;
 
 public class ReservationCheckDAO {
@@ -26,7 +25,7 @@ public class ReservationCheckDAO {
 		try {
 			prop = new Properties();
 			
-			String filePath = ReservationRequestDAO.class.getResource("/edu/kh/hotellala/sql/reservation-sql-pjh.xml").getPath();
+			String filePath = ReservationCheckDAO.class.getResource("/edu/kh/hotellala/sql/reservation-sql-pjh.xml").getPath();
 			
 			prop.loadFromXML(new FileInputStream(filePath));
 			
