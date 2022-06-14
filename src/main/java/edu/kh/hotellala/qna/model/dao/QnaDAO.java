1af43adabad6cbd7c24e495c5d68f722e00ce417
park-sm <nambawon1@naver.com>
2022-06-14 19:37:17 +0900

    회원가입 완료

---
+++ src/main/java/edu/kh/hotellala/qna/model/dao/QnaDAO.java
@@ -0,0 +1,22 @@
+package edu.kh.hotellala.qna.model.dao;
+
+import java.sql.Connection;
+
+import static edu.kh.hotellala.common.JDBCTemplate.*;
+
+import edu.kh.hotellala.qna.model.vo.Qna;
+
+public class QnaDAO {
+
+	public int qnA(Connection conn, Qna q) throws Exception{
+		int result = 0;
+		
+		try {
+			String sql = prop.get
+		}finally {
+			
+		}
+		return result;
+	}
+
+}
