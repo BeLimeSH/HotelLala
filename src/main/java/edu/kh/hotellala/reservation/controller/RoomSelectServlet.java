6fd38240e528b92dd604781dbff882fc53547b54
BeLimeSH <shee5212@naver.com>
2022-06-16 16:53:17 +0900

    객실 상세 조회 추가

--- src/main/java/edu/kh/hotellala/reservation/controller/RoomSelectServlet.java
+++ src/main/java/edu/kh/hotellala/reservation/controller/RoomSelectServlet.java
@@ -1,4 +1,4 @@
-package edu.kh.hotellala.room.controller;
+package edu.kh.hotellala.reservation.controller;
 
 import java.io.IOException;
 
@@ -8,11 +8,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.kh.hotellala.reservation.model.service.ReservationRequestService;
+import edu.kh.hotellala.reservation.model.vo.RoomBoard;
+
 @WebServlet("/room/detail")
 public class RoomSelectServlet extends HttpServlet{
 	//객실 상세보기 팝업창에 객실 정보 불러올 Servlet
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		RoomBoard roomBoard = new RoomBoard();
+		ReservationRequestService service = new ReservationRequestService();
+		
+		try {
+			String type = req.getParameter("type");
+			
+			roomBoard = service.selectRoomDetail(type);
+			
+			System.out.println(roomBoard);
+			
+			req.setAttribute("roomBoard", roomBoard);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 		String path = "/WEB-INF/views/room/roomDetail.jsp";
 		req.getRequestDispatcher(path).forward(req, resp);
--- src/main/java/edu/kh/hotellala/room/model/dao/RoomBoardDAO.java
+++
@@ -1,5 +0,0 @@
-package edu.kh.hotellala.room.model.dao;
-
-public class RoomBoardDAO {
-
-}
